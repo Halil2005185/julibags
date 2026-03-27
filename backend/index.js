4f84348be0cd208587b00559efb62f2affe59073
Halil Ismail <ismailhalil073@gmail.com>
2026-03-27 18:55:53 +0300

    commit

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,10 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
-  }),
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
 );
-
 app.use(express.json());
 app.use("/api/admin", adminProxy);
 
