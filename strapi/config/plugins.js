9bc2c3c408fd27a62f7bb79fa64e1f118eb9964d
Halil Ismail <ismailhalil073@gmail.com>
2026-03-29 14:38:11 +0300

    e

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,30 +1,36 @@
 module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      breakpoints: null,
-      actionOptions: {
-        upload: {
-          folder: "julibags",
-          use_filename: true,
-          unique_filename: true,
-          overwrite: false,
+    upload: {
+        config: {
+            provider: "cloudinary",
+            providerOptions: {
+                cloud_name: env("CLOUDINARY_NAME"),
+                api_key: env("CLOUDINARY_KEY"),
+                api_secret: env("CLOUDINARY_SECRET"),
+            },
+            actionOptions: {
+                upload: {
+                    folder: "julibags",
+                    use_filename: true,
+                    unique_filename: true,
+                    overwrite: false,
+                },
+                uploadStream: {
+                    folder: "julibags",
+                    use_filename: true,
+                    unique_filename: true,
+                    overwrite: false,
+                },
+                delete: {
+                    invalidate: true,
+                },
+            },
         },
-        uploadStream: {
-          folder: "julibags", // ← أضف هذا
-          use_filename: true,
-          unique_filename: true,
-          overwrite: false,
+        breakpoints: {   // ← هنا الصح
+            xlarge: null,
+            large: null,
+            medium: null,
+            small: null,
+            xsmall: null,
         },
-        delete: {
-          invalidate: true, // ← أضف هذا
-        },
-      },
     },
-  },
-});
+})
